67f999732688608c15da98ae4b0950f7185dc25c
Piotr Witaszek <p.a.witaszek@gmail.com>
2022-07-08 11:03:13 +0200

    Changes without loggging

--- Calculator.py
+++ Calculator.py
@@ -14,34 +14,36 @@ def multiply(x, y):
  
 def divide(x, y):
     return x / y
- 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
- 
-while True:
-    choice = input("Enter choice(1/2/3/4): ")
- 
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
- 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
- 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
- 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
- 
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        break
-    else:
-        print("Invalid Input")
 
+
+if __name__ == "__maine__"
+    print("Select operation.")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+ 
+    while True:
+        choice = input("Enter choice(1/2/3/4): ")
+ 
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+ 
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
+ 
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
+ 
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
+ 
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
+            break
+        else:
+            print("Invalid Input")
+# Inne wychodzenie z pętli - chodzi o inny komunikat, który się pojawia?
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
