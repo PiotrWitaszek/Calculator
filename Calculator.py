03dcf2b028097dc29cd4865d36436a4b7bd2608b
Piotr Witaszek <p.a.witaszek@gmail.com>
2022-07-11 08:50:00 +0200

    Change

--- Calculator.py
+++ Calculator.py
@@ -30,22 +30,20 @@ if __name__ == "__main__":
             num2 = float(input("Enter second number: "))
  
         if choice == '1':
-                print(num1, "+", num2, "=", add(num1, num2))
- 
+            print(num1, "+", num2, "=", add(num1, num2)) 
         elif choice == '2':
-                print(num1, "-", num2, "=", subtract(num1, num2))
- 
+            print(num1, "-", num2, "=", subtract(num1, num2))
         elif choice == '3':
-                print(num1, "*", num2, "=", multiply(num1, num2))
- 
+            print(num1, "*", num2, "=", multiply(num1, num2))
         elif choice == '4':
-                print(num1, "/", num2, "=", divide(num1, num2))
+            print(num1, "/", num2, "=", divide(num1, num2))
         elif choice == 'x':
-                exit()
+            exit()
         else:
-                print("Invalid input")
+            print("Invalid input")
 
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s') 
+
     
    
 
